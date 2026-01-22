194179667b34b5c5ba5ed6ff10bfccbf9b87e098
Leon Coto-Rodriguez <leon.coto-rodriguez@weareadaptive.com>
2026-01-22 11:11:14 +0000

    launch two windows with a simpler layout

--- src/ProviderApp.tsx
+++ src/ProviderApp.tsx
@@ -9,33 +9,66 @@ function ProviderApp() {
         await fin.Platform.init({});
 
         const platform = fin.Platform.getCurrentSync();
-        await platform.createWindow({
+
+        const windowWidth = 640
+        const windowHeight = 800
+
+        const windowOptions = {
           url: "http://192.168.68.68:5173/platform-window.html",
-          defaultWidth: 1280,
-          defaultHeight: 800,
-          defaultCentered: true,
+          defaultWidth: windowWidth,
+          defaultHeight: windowHeight,
           autoShow: true,
           frame: false,
           resizable: true,
           contextMenu: true,
-          layout: {
-            content: [
-              {
-                type: "stack",
-                content: [
-                  {
-                    type: "component",
-                    componentName: "view",
-                    componentState: {
-                      name: "main-view",
-                      url: "http://192.168.68.68:5173",
+        }
+
+        await Promise.all([
+          platform.createWindow({
+            ...windowOptions,
+            defaultLeft: 100,
+            defaultTop: 100,
+            layout: {
+              content: [
+                {
+                  type: "stack",
+                  content: [
+                    {
+                      type: "component",
+                      componentName: "view",
+                      componentState: {
+                        name: "main-view-1",
+                        url: "http://192.168.68.68:5173",
+                      },
                     },
-                  },
-                ],
-              },
-            ],
-          },
-        });
+                  ],
+                },
+              ],
+            },
+          }),
+          platform.createWindow({
+            ...windowOptions,
+            defaultLeft: 100 + windowWidth + 20,
+            defaultTop: 100,
+            layout: {
+              content: [
+                {
+                  type: "stack",
+                  content: [
+                    {
+                      type: "component",
+                      componentName: "view",
+                      componentState: {
+                        name: "main-view-2",
+                        url: "http://192.168.68.68:5173",
+                      },
+                    },
+                  ],
+                },
+              ],
+            },
+          }),
+        ])
 
         setIsReady(true);
       }
